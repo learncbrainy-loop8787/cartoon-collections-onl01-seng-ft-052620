c20a430485fc6299c5a05c37ee935aa9cb8a336d
korey ko <koreykendrick1@gmail.com>
2020-06-01 18:40:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,12 +22,6 @@ def summon_captain_planet(array)
 end
 
 summon_captain_planet(planeteer_calls)
-  
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
