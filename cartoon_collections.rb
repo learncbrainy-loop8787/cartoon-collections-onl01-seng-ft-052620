a6e2f674260cc867bbd75e7c0648b44b2f6b31a4
korey ko <koreykendrick1@gmail.com>
2020-06-01 19:02:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -40,13 +40,7 @@ heese_types = ["cheddar", "gouda", "camembert"]
 snacks = ["crackers", "gouda", "thyme"]
 
 def find_the_cheese(array)
-  def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-    array.find do |type|
-    cheese_types.include?(type)
-  end 
-end
-
+  
 find_the_cheese(snacks)
 
 cheese_types = ["cheddar", "gouda", "camembert"]
