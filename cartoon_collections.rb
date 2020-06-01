6fade0945a8066c91217422433e2e3dea08bccba
korey ko <koreykendrick1@gmail.com>
2020-06-01 18:34:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,17 @@ end
 roll_call_dwarves(dwarves)
 
 
+def summon_captain_planet(array)
+  nu_array = []
+  i = 0 
+  while i < array.length 
+    nu_array << array[i].capitalize + "!"
+    i += 1 
+  end 
+  nu_array
+end
 
+summon_captain_planet(planeteer_calls)
   
 end
 
